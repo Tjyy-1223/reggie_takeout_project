9b1a5ebd6c17a03769b60a2b38b0e4a656060739
tianjiangyu <161840237@nuaa.edu.cn>
2022-08-04 19:05:40 +0800

    day3 新增菜品套餐以及菜品套餐的分页展示

---
+++ code/reggie_take_out/src/main/java/com/tjyy/reggie/service/CategoryService.java
@@ -0,0 +1,8 @@
+package com.tjyy.reggie.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.tjyy.reggie.entity.Category;
+import com.tjyy.reggie.entity.Employee;
+
+public interface CategoryService extends IService<Category> {
+}
---
+++ code/reggie_take_out/src/main/java/com/tjyy/reggie/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,11 @@
+package com.tjyy.reggie.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.tjyy.reggie.entity.Category;
+import com.tjyy.reggie.mapper.CategoryMapper;
+import com.tjyy.reggie.service.CategoryService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryServiceImpl extends ServiceImpl<CategoryMapper, Category> implements CategoryService {
+}
